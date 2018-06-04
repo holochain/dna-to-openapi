af545f7d46f736c1110c18f2ffdbdad3ba7c6efc
neonphog <neonphog@gmail.com>
2018-06-04 15:11:31 -0600

    (squash) initial working proof of concept

---
+++ README.md
@@ -0,0 +1,48 @@
+# dna-to-openapi
+
+given holochain dna, generate a swagger / openapi spec file
+
+## usage
+
+### commandline
+
+```
+$ ./bin/dna-to-openapi -h
+Usage dna-to-openapi [options]
+
+Options:
+  -p, --path     specify a path (otherwise ".")
+  -s, --spec     generates an open-api spec file ("-" for stdout)
+  -d, --doc      generates a static swagger-ui html doc ("-" for stdout)
+  -h, --help     Show help                                             [boolean]
+  -v, --version  Show version number                                   [boolean]
+
+Examples:
+  dna-to-openapi                            lints the given holo app / dna
+  dna-to-openapi --path <path>              to specify a path (otherwise ".")
+  dna-to-openapi --spec <spec-file.json>    generates an open-api spec
+  dna-to-openapi --doc <api-doc.html>       generates static swagger-ui html doc
+  dna-to-openapi --spec <spec-file.json>    does both
+  --doc <api-doc.html>
+```
+
+### as a library
+
+```javascript
+const dna = require('dna-to-openapi')
+
+// 1 - lint
+const lintResults = dna.lint(dnaJsonObject, schemasObject)
+
+// if there are errors (lintResults.errors), abort here.
+// generating a spec file with lint errors may not work
+if (lintResults.errors.length > 0) {
+  process.exit(1)
+}
+
+// 2 - generate openapi spec
+const specJson = dna.convert(dnaJsonObject, schemasObject)
+
+// 3 - generate swagger-ui html
+const swaggerHtml = dna.genDocs(specJson)
+```
---
+++ bin/dna-to-openapi.test.js
@@ -0,0 +1,75 @@
+'use strict'
+
+const fs = require('fs')
+const path = require('path')
+const childProcess = require('child_process')
+
+const expect = require('chai').expect
+const tmp = require('tmp')
+tmp.setGracefulCleanup()
+
+const version = require('../package').version
+
+const TIMEOUT = 5000
+
+function exec (...args) {
+  return new Promise((resolve, reject) => {
+    try {
+      const here = path.resolve(__dirname)
+      args.unshift(path.resolve(here, '../bin/dna-to-openapi'))
+      args.unshift('node')
+      childProcess.exec("'" + args.join("' '") + "'", {
+        cwd: here,
+        timeout: TIMEOUT
+      }, (err, stdout, stderr) => {
+        if (err) {
+          return reject(new Error(JSON.stringify({
+            err: err,
+            stderr: stderr,
+            stdout: stdout
+          })))
+        }
+        resolve({ stdout, stderr })
+      })
+    } catch (e) {
+      reject(e)
+    }
+  })
+}
+
+describe('dna-to-openapi commandline Suite', () => {
+  it('-v should print version', async () => {
+    const res = await exec('-v')
+    expect(res.stdout).equals(version + '\n')
+  }).timeout(TIMEOUT)
+
+  it('should fail without dna', async () => {
+    try {
+      await exec()
+    } catch (e) {
+      return
+    }
+    throw new Error('expected throw, but did not')
+  }).timeout(TIMEOUT)
+
+  it('should lint w/o warnings or errors', async () => {
+    const res = await exec('--path', '../test')
+    expect(res.stderr).equals('')
+  }).timeout(TIMEOUT)
+
+  it('should generate spec', async () => {
+    const tfile = tmp.tmpNameSync()
+    await exec('--path', '../test', '--spec', tfile)
+    const data = fs.readFileSync(tfile).toString()
+    fs.unlinkSync(tfile)
+    expect(data).contains('openapi').contains('3.0.0')
+  }).timeout(TIMEOUT)
+
+  it('should generate doc', async () => {
+    const tfile = tmp.tmpNameSync()
+    await exec('--path', '../test', '--doc', tfile)
+    const data = fs.readFileSync(tfile).toString()
+    fs.unlinkSync(tfile)
+    expect(data).contains('openapi').contains('#swagger-ui')
+  }).timeout(TIMEOUT)
+})
